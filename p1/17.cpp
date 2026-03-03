a60a7c7efb188b0bb20050caa6c9033d709bae20
ElonFreeman <zhanghao112358@outlook.com>
2026-03-03 19:58:58 +0800

    26030302

--- p1/17.cpp
+++ p1/17.cpp
@@ -1,3 +1,37 @@
 #include<iostream>
 using namespace std;
 
+int main(void)
+{
+    int a=48,b=12;
+    int tma=a,tmb=b;
+    if(a<b)
+    {
+        int tmp=a;
+        a=b;
+        b=tmp;
+    }
+
+    int GCD=0;
+    /*GCD*/
+    while(1)
+    {
+        if(a%b!=0)
+        {
+            int tmp=b;
+            b=a%b,a=tmp;
+        }
+        else if(a%b==0)
+        {
+            GCD=b;
+            break;
+        }
+    }
+
+    /*LCD*/
+    int LCM=(tma*tmb)/GCD;
+
+    cout << GCD << ' ' << LCM << endl;
+
+    return 0;
+}
--- p1/18.cpp
+++ p1/18.cpp
@@ -3,15 +3,7 @@
 #include<algorithm>
 using namespace std;
 
-/*int compare(const void* a,const void* b)
-{
-    int ia = *(int*)a;
-    int ib = *(int*)b;
-    return ia - ib; 
-    const char* sa = (*(string*)a).c_str();
-    const char* sb = (*(string*)b).c_str();
-    return strcmp(sa, sb);
-}*/
+
 
 void sort(string *strs,int volume)
 {
@@ -20,19 +12,11 @@ void sort(string *strs,int volume)
 
 int main(void)
 {
-    string strs[10]=
+    string strs[10]={};
+    for(int i=0;i<10;i++)
     {
-        "Hello",                    // 普通字符串
-        "World",                     // 普通字符串
-        "12345",                     // 数字字符串
-        "!@#$%",                     // 特殊字符
-        "",                          // 空字符串
-        "a",                         // 单字符
-        "This is a longer string",   // 长字符串
-        "UPPERCASE",                 // 大写
-        "lowercase",                 // 小写
-        "Mixed Case 123"             // 混合
-    };
+        cin >> strs[i];
+    }
     
     sort(strs,sizeof(strs)/sizeof(strs[0]));
 
